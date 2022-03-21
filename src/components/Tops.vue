b2987d919b7f8bc5da611188aefcfa2723722dda
Daisyyyyy0 <soniahsiao16@gmail.com>
2022-03-21 17:56:44 +0800

    still wrong

---
+++ src/components/Tops.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+    props:{
+        initialTops:{
+            type:Array,
+            required: true,
+        }  
+    },
+    data(){
+        return{
+            users: this.initialTops
+        }
+    },
+    methods: {
+      follow(){
+        this.users = {
+          ...this.users,
+          isFollowed:true
+        }
+      },
+      unfollow(){
+        this.users = {
+          ...this.users,
+          isFollowed:false
+        }
+      },
+    },      
+}
+</script>
+
+<template>
+      <div class="row text-center">
+      <div class="col-3" v-for="user in users" :key="user.id">
+        <router-link to="/">
+          <img
+            src="http://via.placeholder.com/300x300?text=No+Image"
+            width="140px"
+            height="140px"
+          >
+        </router-link>
+        <h2>{{ user.name }}</h2>
+        <span class="badge badge-secondary">追蹤人數： {{user.FollowerCount}} </span>
+        <p class="mt-3">
+          <button
+            type="button"
+            class="btn btn-danger"
+            v-if="user.isFollowed"
+            @click.stop.prevent="unfollow"
+          >
+            取消追蹤
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            v-else
+            @click.stop.prevent="follow"
+          >
+            追蹤
+          </button>
+        </p>
+      </div>
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -1,6 +1,7 @@
 
 <script>
 import NavTabs from "./../components/NavTabs.vue";
+import Tops from "./../components/Tops.vue";
 
 const dummyData = {
     "users": [
@@ -49,6 +50,7 @@ export default {
     name: 'users-top',
     components:{
         NavTabs,
+        Tops,
     },
     data(){
       return{
@@ -57,21 +59,11 @@ export default {
     },
     methods: {
       fetchUsersTop(){
+        const { users } = dummyData
         this.users = dummyData.users
         // console.log(...this.users)
       },
-      follow(){
-        this.users = {
-          ...this.users,
-          isFollowed:true
-        }
-      },
-      unfollow(){
-        this.users = {
-          ...this.users,
-          isFollowed:false
-        }
-      },
+
     },
     created(){
       this.fetchUsersTop()
@@ -86,37 +78,7 @@ export default {
       美食達人
     </h1>
     <hr>
-    <div class="row text-center">
-      <div class="col-3" v-for="user in users" :key="user.id">
-        <router-link to="/">
-          <img
-            src="http://via.placeholder.com/300x300?text=No+Image"
-            width="140px"
-            height="140px"
-          >
-        </router-link>
-        <h2>{{ user.name }}</h2>
-        <span class="badge badge-secondary">追蹤人數： {{user.FollowerCount}} </span>
-        <p class="mt-3">
-          <button
-            type="button"
-            class="btn btn-danger"
-            v-if="user.isFollowed"
-            @click.stop.prevent="unfollow"
-          >
-            取消追蹤
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary"
-            v-else
-            @click.stop.prevent="follow"
-          >
-            追蹤
-          </button>
-        </p>
-      </div>
-    </div>
+    <Tops :initial-tops="users"/>
   </div>
 </template>
 
