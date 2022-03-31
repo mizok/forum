c715e8db5450f878a6ba0a5f3b9445cca148d976
Daisy <soniahsiao16@gmail.com>
2022-03-31 21:57:14 +0800

    add error catch

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -40,6 +40,7 @@
         class="btn btn-lg btn-primary btn-block mb-3"
         type="submit"
         @click.prevent.stop="handleSubmit"
+        :disabled="isProcessing"
       >
         Submit
       </button>
@@ -61,11 +62,14 @@
 
 <script>
 import authorizationAPI from './../apis/authorization'
+import { Toast } from './../utils/helpers'
+
 export default {
   data(){
     return {
       email: '',
       password: '',
+      isProcessing: false,
     }
   },
   methods:{
@@ -74,20 +78,49 @@ export default {
       //   email: this.email,
       //   password: this.password
       // })
+
+      // 如果 email 或 password 為空，則使用 Toast 提示
+      // 然後 return 不繼續往後執行
+      if (!this.email || !this.password) {
+          Toast.fire({
+              icon: 'warning',
+              title: '請填入 email 和 password'
+          })
+          return
+      }
+
+      this.isProcessing = true
+
       authorizationAPI.signIn({
         email: this.email,
         password: this.password
-      }).then(response => {
-        // TODO: 取得 API 請求後的資料
-        // 取得 API 請求後的資料
-        const { data } = response
-        console.log('response', response)
-        // 將 token 存放在 localStorage 內
-        localStorage.setItem('token', data.token)
-        // 成功登入後轉址到餐廳首頁
-        this.$router.push('/restaurants')
       })
-      // console.log('data', data)  
+      .then(response => {
+          // TODO: 取得 API 請求後的資料
+          // 取得 API 請求後的資料
+          const { data } = response
+          if(data.status !== `success`){
+              throw new Error(data.message)
+          }
+          // 將 token 存放在 localStorage 內
+          localStorage.setItem('token', data.token)
+          // 成功登入後轉址到餐廳首頁
+          this.$router.push('/restaurants')
+      })
+      .catch(error => {
+          // 將密碼欄位清空
+          this.password = ''
+          // 顯示錯誤提示
+          Toast.fire({
+              icon: 'warning',
+              title: '您輸入了的帳號或密碼錯誤'
+          })
+           // 因為登入失敗，所以要把按鈕狀態還原
+          this.isProcessing = true
+
+          console.log('error',error);
+      })
+      
     },
   }, 
 }
